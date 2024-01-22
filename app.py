fe94c30905f8e6b317732ca06df31b3faff5f4f6
yzuw <1046031241@qq.com>
2024-01-22 16:36:34 +0800

    添加base模板和404，使用模板继承

--- app.py
+++ app.py
@@ -68,8 +68,20 @@ def forge():
     click.echo('添加完毕~')
 
 
+@app.context_processor
+def inject_user():
+    user = User.query.first()
+    return dict(user=user)
+
+
 @app.route('/')
 def index():
     user = User.query.first()
     movies = Movie.query.all()
-    return render_template('index.html', user=user, movies=movies)
+    return render_template('index.html', movies=movies)
+
+
+@app.errorhandler(404)  # 传入要处理的错误代码
+def page_not_found(e):  # 接受异常对象作为参数
+    user = User.query.first()
+    return render_template('404.html'), 404  # 返回模板和状态码
