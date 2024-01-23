ec3dca9df192507e361bd9d223b9b98f804d9e72
yzuw <1046031241@qq.com>
2024-01-23 13:36:01 +0800

    部署

--- myproject/__init__.py
+++ myproject/__init__.py
@@ -15,7 +15,8 @@ if WIN:  # 如果是 Windows 系统，使用三个斜线
 else:  # 否则使用四个斜线
     prefix = 'sqlite:////'
 # 在扩展类实例化前加载配置
-app.config['SQLALCHEMY_DATABASE_URI'] = prefix + os.path.join(os.path.dirname(app.root_path), 'data.db')
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY', 'dev')
+app.config['SQLALCHEMY_DATABASE_URI'] = prefix + os.path.join(os.path.dirname(app.root_path), os.getenv('DATABASE_FILE', 'data.db'))
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # 关闭对模型修改的监控
 # 初始化扩展，传入程序实例 app
 db = SQLAlchemy(app)
